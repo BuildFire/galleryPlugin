9a11a90ab15747ff1766d4b3aa01928674d67f71
Mahmoud AlSharif <mahmoud.k.alsharif@gmail.com>
2023-08-28 19:19:27 +0300

    fix(seeder): better ai messageing and processing

--- src/control/content/containers/Home.js
+++ src/control/content/containers/Home.js
@@ -15,71 +15,114 @@ const Home = ({
   handleReorder,
   saveSampleAiData
 }) => {
+  // init state seeder
   if (loaded && showEmptyState) {
-    const jsonTemplate = {
-      images: [],
-      folders: [
-        {
-          name: ""
-        }
-      ]
+    const generateRandomNumber = () => {
+      const min = 1000000000000; // Smallest 13-digit number
+      const max = 9999999999999; // Largest 13-digit number
+      return Math.floor(Math.random() * (max - min + 1)) + min;
     };
-    const prepareData = (result, callback) => {
-      const imagePromises = result.data.images.map(
-        src => new Promise(resolve => {
-          const image = new Image();
-          image.onload = () => resolve(image);
-          image.src = src;
-          image.originalSrc = src;
-        })
-      );
 
-      Promise.all(imagePromises).then(imgs => {
-        const images = imgs.map(
-          ({ naturalWidth, naturalHeight, originalSrc }) => new Img({
-            src: originalSrc,
-            width: naturalWidth,
-            height: naturalHeight
-          })
-        );
-        const folders = result.data.folders.map((el, index) => {
-          el.images = [images[index]];
-          return new Folder(el);
-        });
-
-        callback({images, folders});
-      });
-    }
     const seeder = new buildfire.components.aiStateSeeder({
       generateOptions: {
         userMessage: 'Provide me with images that relate to [app-interests]',
-        systemMessage: 'Generate up to five image urls based on the input, use source.unsplash.com for image URLs. Create equal number of folders associated with the topic as images array.',
-        jsonTemplate,
+        systemMessage: 'Use source.unsplash.com for image URLs. Do not remove white spaces in the subject string.',
+        jsonTemplate: {
+          images: [],
+          topic: '',
+        },
         callback: (err, result) => {
-          prepareData(result, (data) => {
-            saveSampleAiData(data, "generate");
+          const promises = result.data.images.map(
+            item => new Promise(resolve => {
+              const image = new Image();
+              image.onload = () => resolve(image);
+              // adding cache buster to fix image duplication
+              image.src = `${item.url}?v=${generateRandomNumber()}`;
+              image.originalSrc = item.url;
+            })
+          );
+
+          Promise.all(promises).then(items => {
+            const processedImages = items.map(
+              ({ naturalWidth, naturalHeight, originalSrc }) => new Img({
+                src: originalSrc,
+                width: naturalWidth,
+                height: naturalHeight
+              })
+            );
+
+            const processedFolders = [
+              new Folder({ name: result.data.topic, images: processedImages })
+            ];
+            const data = {
+              images: processedImages,
+              folders: processedFolders,
+            };
+
+            saveSampleAiData(data, 'generate');
             seeder.requestResult.complete();
           });
-        }
+        },
       },
       importOptions: {
-        jsonTemplate,
-        sampleCSV: 'https://source.unsplash.com/featured/?sunny, https://source.unsplash.com/featured/?rainy, https://source.unsplash.com/featured/?cloudy\n\rSunny, Rainy, Cloudy',
-        systemMessage: `First row are images, second row are folders.`,
+        jsonTemplate: {
+          folders: [{ name: '', images: [{ url: '' }] }]
+        },
+        sampleCSV: 'Weather: https://source.unsplash.com/featured/?sunny\n'
+          + 'Weather:https://source.unsplash.com/featured/?rainy\n'
+          + 'City:https://source.unsplash.com/featured/?street',
+        systemMessage: 'Each row contains a folder and image URL that should belong to.',
         callback: (err, result) => {
-          prepareData(result, (data) => {
-            if (seeder.requestResult.resetData)
-              saveSampleAiData(data, "generate");
-            else saveSampleAiData(data, "import");
+          const promises = result.data.folders.reduce(
+            (accumulator, currentValue) => {
+              currentValue.images.forEach(i => {
+                accumulator.push(new Promise(resolve => {
+                  const image = new Image();
+                  image.onload = () => resolve(image);
+                  image.src = i.url;
+                  image.setAttribute('data-folder', currentValue.name);
+                  image.originalSrc = i.url;
+                }));
+              });
+              return accumulator;
+            },
+            [],
+          );
 
+          Promise.all(promises).then(items => {
+
+            const processedFolders = [];
+            const processedImages = [];
+
+            items.forEach(item => {
+              const image = new Img({
+                src: item.originalSrc,
+                width: item.naturalWidth,
+                height: item.naturalHeight
+              });
+              processedImages.push(image);
+              const folder = processedFolders.find(f => f.name === item.getAttribute('data-folder'));
+
+              if (!folder) {
+                processedFolders.push(new Folder({ name: item.getAttribute('data-folder'), images: [image] }));
+              } else {
+                folder.images.push(image);
+              }
+            });
+
+            const data = { images: processedImages, folders: processedFolders }
+
+            if (seeder.requestResult.resetData) {
+              saveSampleAiData(data, 'generate');
+            } else {
+              saveSampleAiData(data, 'import');
+            }
             seeder.requestResult.complete();
           });
         }
       }
     })
-      .smartShowEmptyState({ selector: "#ai-seeder", }, (err, result) => {
-        
-      });
+      .smartShowEmptyState();
   }
   return (
     <>
@@ -103,7 +146,7 @@ const Home = ({
           />
         </>
       : <></>}
-      
+
     </>
   )
 }
